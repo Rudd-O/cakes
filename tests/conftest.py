448fc6273ca144b772ac1bc326623461ee86f4ef
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-06-20 22:02:33 +0000

    Initial commit.

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import sys
+from os.path import dirname as d
+from os.path import abspath, join, pathsep
+
+root_dir = d(d(abspath(__file__)))
+env = join(root_dir, ".env")
+with open(env) as f:
+
+    def ispypath(s: str) -> bool:
+        return s.startswith("PYTHONPATH")
+
+    pythonpaths = [x[11:].rstrip() for x in f.readlines() if ispypath(x)]
+    if pythonpaths:
+        pythonpath = pythonpaths[0]
+        pythonpath = pythonpath.replace("${env:PROJ_DIR}", root_dir)
+        pythonpaths = pythonpath.split(pathsep)
+        for p in pythonpaths:
+            sys.path.append(p)
+sys.path.append(join(root_dir, "src"))
